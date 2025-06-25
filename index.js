f9ecb6859ddff08203e05f1c521b18b3eb2adc43
codegecko <johnugochi596@gmail.com>
2025-06-25 17:21:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,3 +49,15 @@ fetch("https://apis.scrimba.com/jsonplaceholder/todos", {
 })
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+
+  /**
+ * Challenge: GET all the comments from the blog post with ID of 2 and log to the console
+ *
+ * BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ * Endpoint: ??? (Check JSON Placeholder docs: https://jsonplaceholder.typicode.com/guide/ and look for the "Listing nested resources" section)
+ */
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts/2/comments")
+    .then(res => res.json())
+    .then(data => console.log(data))
